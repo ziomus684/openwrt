8bed1461df5a073c3aaf876fcdfc00a0727c2ba4
Ansuel Smith <ansuelsmth@gmail.com>
2022-05-21 21:04:19 +0200

    ipq807x: Move Xiaomi 3600 enlarge single rootfs
    Rework partition logic to expand the rootfs.
    - Drop dual partition support
    - Enforce single partition and load always the first
      partition
    - Expand the roots from 10mb or less to 200+mb and
      use full nand
    
    To make this change we had to drop smem partition layout
    and use fixed partition layout by providing our own.
    Due to Xiaomi bootloader limitation, some hacks are needed
    to make this work correctly.
    The hardcoded bootmiwifi command does tons of change to the
    installed system. One of them is putting in the partitions
    node the smem partition by overwriting them. This was found
    by Robi and was ""fixed"" by providing a dummy partitions node.
    
    Another problem comes from applying an ubi layout that is different
    than the one uboot expects, because of this when the command is
    called, the rootfs_data is resized to the smem partition size
    instead of the one provided by the kernel. This makes effectively
    any fixed partition tweak useless since uboot will revert it anyway in
    one or another way,
    
    To fix this additional problem we decided to implement a new logic.
    We put the kernel in a dedicated ubi partition that is the rootfs
    partition uboot expect the entire system to be placed on (35mb).
    We then leave a 128KiB of wasted space after the first rootfs to
    make uboot fail if he does have the glorious idea of trying to load
    the second partition. Finally we put rootfs and rootfs_data to the
    rest of the nand. This effectively solve all the problems and permit
    a correct expansion of the nand by bypassing all the uboot limitation,
    at the expense of some mb wasted.
    
    To install openwrt on this new partition layout, UART access is mandatory
    but this is to prevent any sort of error where an user would cause
    a brick on rootfs getting filled. (On an usable system with normal
    package, you would get 9 or 8 mb of free space and that is with basic
    package... adding additional stuff and your rootfs is full)
    
    To install it you have to load a initramfs image (once you have enabled
    uboot serial access)
    (example command)
    tftpboot 50000000 192.168.1.10:ipq807x/generic/openwrt-ipq807x-generic-xiaomi_ax3600-initramfs-fit-uImage.itb; bootm
    
    Once the system has loaded simply load the 2 .ubi factory files with SCP and run
    the following command:
    ubiformat /dev/mtd11 -f /tmp/openwrt-ipq807x-generic-xiaomi_ax3600-squashfs-nand-factory-kernel.ubi
    ubiformat /dev/mtd12 -f /tmp/openwrt-ipq807x-generic-xiaomi_ax3600-squashfs-nand-factory-rootfs.ubi
    fw_setenv flag_boot_rootfs 0
    fw_setenv flag_last_success 0
    fw_setenv flag_boot_success 0
    fw_setenv flag_try_sys1_failed 0
    fw_setenv flag_try_sys2_failed 1
    
    Now you can reboot the router and system will be loaded
    with the new partition layout.
    
    Signed-off-by: Ansuel Smith <ansuelsmth@gmail.com>

--- target/linux/ipq807x/image/generic.mk
+++ target/linux/ipq807x/image/generic.mk
@@ -4,6 +4,12 @@ define Device/FitImage
 	KERNEL_NAME := Image
 endef
 
+define Device/FitImageUbinize
+	KERNEL_SUFFIX := -fit-uImage.itb
+	KERNEL = kernel-bin | gzip | fit gzip $$(DEVICE_DTS_DIR)/$$(DEVICE_DTS).dtb | ubinize-kernel
+	KERNEL_NAME := Image
+endef
+
 define Device/FitImageLzma
 	KERNEL_SUFFIX := -fit-uImage.itb
 	KERNEL = kernel-bin | lzma | fit lzma $$(DEVICE_DTS_DIR)/$$(DEVICE_DTS).dtb
@@ -23,11 +29,11 @@ define Device/UbiFit
 	IMAGE/nand-sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
 
-define Device/UbiFit-ipq
-        KERNEL_IN_UBI := 1
-        IMAGES := nand-factory.ubi nand-sysupgrade.bin
-        IMAGE/nand-factory.ubi := append-ubi | qsdk-ipq-factory-nand
-        IMAGE/nand-sysupgrade.bin := sysupgrade-tar | append-metadata
+define Device/UbiFitSplit
+	IMAGES := nand-factory-kernel.ubi nand-factory-rootfs.ubi nand-sysupgrade.bin
+	IMAGE/nand-factory-kernel.ubi := append-kernel
+	IMAGE/nand-factory-rootfs.ubi := append-ubi
+	IMAGE/nand-sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
 
 define Device/edgecore_eap102
@@ -72,18 +78,24 @@ endef
 TARGET_DEVICES += qnap_301w
 
 define Device/redmi_ax6
-	$(call Device/xiaomi_ax3600)
+	$(call Device/FitImage)
+	$(call Device/UbiFit)
 	DEVICE_VENDOR := Redmi
 	DEVICE_MODEL := AX6
+	BLOCKSIZE := 128k
+	PAGESIZE := 2048
+	DEVICE_DTS_CONFIG := config@ac04
+	SOC := ipq8071
 	DEVICE_PACKAGES := ipq-wifi-redmi_ax6
 endef
 TARGET_DEVICES += redmi_ax6
 
 define Device/xiaomi_ax3600
-	$(call Device/FitImage)
-	$(call Device/UbiFit)
+	$(call Device/FitImageUbinize)
+	$(call Device/UbiFitSplit)
 	DEVICE_VENDOR := Xiaomi
 	DEVICE_MODEL := AX3600
+	KERNEL_SIZE := 34816k
 	BLOCKSIZE := 128k
 	PAGESIZE := 2048
 	DEVICE_DTS_CONFIG := config@ac04
